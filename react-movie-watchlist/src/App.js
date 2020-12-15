c8700ef7ad03eb20b2231f3721eab0f8a4087e92
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-12-15 12:50:52 -0600

    Setup the add.js file.

--- react-movie-watchlist/src/App.js
+++ react-movie-watchlist/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {Header} from './components/Header';
-import {Watchlist} from './components/Watchlist';
-import {Watched} from './components/Watched';
-import {Add} from './components/Add';
-import './App.css';
-import './lib/font-awesome/css/all.min.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Watchlist } from "./components/Watchlist";
+import { Watched } from "./components/Watched";
+import { Add } from "./components/Add";
+import "./App.css";
+import "./lib/font-awesome/css/all.min.css";
+
+// import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
